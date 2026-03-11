435168f3039d48b973c70b1ae65ca9c46f17fe6a
divvyesk <divvyesk2428@gmail.com>
2026-03-11 16:39:53 +0530

    subscription model file added

---
+++ src/models/subscription.models.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        ref: "User"
+    }
+}, {timestamps: true})
+
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
